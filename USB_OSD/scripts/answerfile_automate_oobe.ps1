d47302723dec986498a7213c84c65cb7d7bdffc0
whyso <ygxxii@gmail.com>
2020-08-07 17:19:35 +0800

    New scripts: answerfile_automate_oobe.ps1 & answerfile_automate_windowssetup.ps1

---
+++ USB_OSD/scripts/answerfile_automate_oobe.ps1
@@ -0,0 +1,2 @@
+
+# Automate OOBE | Microsoft Docs https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/automate-oobe
---
+++ USB_OSD/scripts/answerfile_automate_windowssetup.ps1
@@ -0,0 +1,2 @@
+
+# Automate Windows Setup | Microsoft Docs https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/automate-windows-setup
